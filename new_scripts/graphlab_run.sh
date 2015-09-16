04824d742db162cbb3c59ffc0add161d8fac5326
shuangsong1990 <songshuang1990@gmail.com>
2015-09-15 21:20:48 -0500

    script folder update

---
+++ new_scripts/graphlab_run.sh
@@ -0,0 +1,2 @@
+#example run command:
+#mpiexec --machinefile ../new_machinefile/machinefile ./release/toolkits/graph_analytics/pagerank --graph="../datasets/amazon" --format=tsv --iterations=35 --graph_opts="ingress=random" --engine sync
